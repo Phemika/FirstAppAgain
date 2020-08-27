8c275f6501acd8f71ce96ac91b5a3194c143e77c
a <pamika.si@rmuti.ac.th>
2020-08-28 01:42:40 +0700

    Create the Database class

---
+++ app/src/main/java/com/example/firstappagain/database/Database.kt
@@ -0,0 +1,34 @@
+package com.example.firstappagain.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import chimjan.xkalux.mobilehomework_firstapp.database.Contact
+
+
+@Database(entities = [Contact::class], version = 1, exportSchema = false)
+abstract class Database : RoomDatabase(){
+    abstract val databaseDao: DatabaseDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: com.example.firstappagain.database.Database? = null
+        fun getInstance(context: Context): com.example.firstappagain.database.Database {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        com.example.firstappagain.database.Database::class.java,
+                        "database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
